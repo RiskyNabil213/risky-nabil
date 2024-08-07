d3490e434637c55e3ef69a11e7b009b9c5aa4b73
RiskyNabil213 <risky_pahlevi_33rpl@student.smktelkom-mlg.sch.id>
2024-08-07 14:47:39 +0700

    Add files via upload

---
+++ drivertessiswa.java
@@ -0,0 +1,73 @@
+public class drivertessiswa {
+    public static void main(String[] args) {
+        // membuat obyek
+        guru bupasha = new guru();
+        guru bujevi = new guru();
+        guru pakdiaur = new guru();
+        guru buaulia = new guru();
+        guru bufirda = new guru();
+
+        bupasha.id = 1;
+        bupasha.nama = "Bu Pashatania";
+        bupasha.mapel = "produktif";
+        bupasha.alamat = "malang";
+        
+        System.out.println("Ini data Bu Pasha");
+        System.out.println(bupasha.id);
+        System.out.println(bupasha.nama);
+        System.out.println(bupasha.mapel);
+        System.out.println(bupasha.alamat);
+
+        bujevi.id = 21;
+        bujevi.nama = "Bu jevi";
+        bujevi.mapel = "Bahasa Inggris";
+        bujevi.alamat = "Surabaya";
+
+        System.out.println("data bu jevi");
+        System.out.println(bujevi.id);
+        System.out.println(bujevi.nama);
+        System.out.println(bujevi.mapel);
+        System.out.println(bujevi.alamat);
+        
+        
+        pakdiaur.id = 22;
+        pakdiaur.nama= "pak diauraja";
+        pakdiaur.mapel= "fisika";
+        pakdiaur.alamat= "mojokerto";
+
+        System.out.println("data pak diaur");
+        System.out.println(pakdiaur.id);
+        System.out.println(pakdiaur.nama);
+        System.out.println(pakdiaur.mapel);
+        System.out.println(pakdiaur.alamat);
+
+    
+        buaulia.id = 23;
+        buaulia.nama= "Bu aulia";
+        buaulia.mapel= "produktif";
+        buaulia.alamat= "malang sawojajar";
+
+        System.out.println("data bu aulia");
+        System.out.println(buaulia.id);
+        System.out.println(buaulia.nama);
+        System.out.println(buaulia.mapel);
+        System.out.println(buaulia.alamat);
+
+
+        bufirda.id = 24;
+        bufirda.nama= "Bu Firdausa";
+        bufirda.mapel= "Dasar Program Keahlian";
+        bufirda.alamat= "jakarta";
+
+        System.out.println("data bu Firda");
+        System.out.println(bufirda.id);
+        System.out.println(bufirda.nama);
+        System.out.println(bufirda.mapel);
+        System.out.println(bufirda.alamat);
+    
+
+        
+        
+    
+    }
+}
---
+++ guru.java
@@ -0,0 +1,21 @@
+// guru class
+public class guru{
+    int id;
+    String nama;
+    String mapel;
+    String alamat;
+
+    // constructur nama sama dgn mapel
+    public guru(){
+        id = 0;
+        nama = "";
+        mapel = "";
+        alamat = "";
+
+    }
+
+
+}
+
+  
+    
